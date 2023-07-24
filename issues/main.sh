cc270177d3addaad324c56bbf927936548d68e42
Bill White <billw@wolfram.com>
2023-07-24 04:45:05 -0500

    Adding 1971

--- issues/main.sh
+++ issues/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-year=1972
+year=1971
 
 echo "bringing files from $year into the repo..."
 rsync -a /Users/billw/Dropbox/projects/Findlay-Enterprise/enterprise/$year/ /Users/billw/git/findlay-enterprise/issues/$year
